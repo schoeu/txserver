e9dc19d9a719d265817e5c63b0d804797cd7b997
schoeu <schoeu89@gmail.com>
2018-03-05 19:24:47 +0800

    add token func.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import json
 import os
 import time
-from flask import Flask, request
+import md5
+from flask import Flask, request, jsonify
+import conf
 
 app = Flask(__name__)
 
-#app.debug = True
+# app.debug = True
 
 port = 8918
 host = '0.0.0.0'
@@ -20,17 +22,24 @@ def update():
     if request.method == 'POST':
         data = request.get_data()
         if data == '':
-            return 'Wrong json string.'
+            return get_wrong_msg('Wrong json string.')
         d = json.loads(data)
+
+        token =  d['auth_token']
+        timestamp = d['timestamp']
+        isOk = check_token(str(timestamp) + conf.psw, token)
+        if not isOk:
+            return get_wrong_msg('Wrong token.')
+
         if 'blackurllist' not in d:
-            return 'No blackurllist data.'
+            return get_wrong_msg('No blackurllist data.')
         url_list = d['blackurllist']
 
         if len(url_list) == 0:
-            return 'No url list data.'
+            return get_wrong_msg('No url list data.')
 
         store_data(url_list)
-    return 'test'
+    return get_ok_state()
 
 def store_data(data):
     rs_path = os.path.join(os.getcwd(), tmp_dir_name)
@@ -53,6 +62,32 @@ def make_sure_dir(path):
         pass
     else:
         os.mkdir(path)
+
+def check_token(t, token):
+    md5_str = get_md5_str(t)
+    rs = md5_str[0:16]
+    print('rs',rs)
+    if rs == token:
+        return True
+    else:
+        return False
+
+def get_md5_str(s):
+    m = md5.new()
+    m.update(s)
+    return m.hexdigest()
+
+def get_wrong_msg(msg):
+    rs = {}
+    rs['result'] = 'fail'
+    rs['msg'] = msg
+    return jsonify(rs)
+
+def get_ok_state():
+    rs = {}
+    rs['result'] = 'ok'
+    rs['msg'] = 'ok'
+    return jsonify(rs)
 
 if __name__ == '__main__':
     app.run(host=host, port=port)
